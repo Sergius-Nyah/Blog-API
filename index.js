0438f2623416fccb1849299a6053b8ceceea18c1
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-29 16:54:01 +0100

    Add comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 4000;
 
-// In-memory data store
+// Sample POSTS data
 let posts = [
   {
     id: 1,
@@ -12,7 +12,7 @@ let posts = [
     content:
       "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
     author: "Alex Thompson",
-    date: "2023-08-01T10:00:00Z",
+    date: "2023-08-01T10:00:00Z", 
   },
   {
     id: 2,
@@ -38,19 +38,19 @@ let lastId = 3;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//CHALLENGE 1: GET All posts
+//GET All posts
 app.get("/posts", (req, res) => {
   console.log(posts);
   res.json(posts); 
 }); 
-//CHALLENGE 2: GET a specific post by id
+// GET a specific post by id
 app.get("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
   if (!post) return res.status(404).json({ message: "Post not found" });
   res.json(post);
 });
 
-//CHALLENGE 3: POST a new post
+// POST a new post
 app.post("/posts", (req, res) => {
   const newId = lastId += 1;
   const post = {
@@ -65,7 +65,7 @@ app.post("/posts", (req, res) => {
   res.status(201).json(post);
 });
 
-//CHALLENGE 4: PATCH a post when you just want to update one parameter.
+// PATCH a post when you just want to update one parameter.
 // User provides required ID in this case. 
 app.patch("/posts/:id", (req, res) => {
   const post = posts.find((p) => p.id === parseInt(req.params.id));
@@ -79,7 +79,7 @@ app.patch("/posts/:id", (req, res) => {
 });
 
 
-//CHALLENGE 5: DELETE a specific post by providing the post id(Still from user).
+// DELETE a specific post by providing the post id(Still from user).
 app.delete("/posts/:id", (req, res) => {
   const index = posts.findIndex((p) => p.id === parseInt(req.params.id));
   if (index === -1) return res.status(404).json({ message: "Post not found" });
